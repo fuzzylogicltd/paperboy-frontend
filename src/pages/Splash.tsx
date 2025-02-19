629ef7d46b31fc2c3cdb9a2936c32fd761a8268c
Lazar <lazar@fuzzylogic.ltd>
2025-02-19 17:26:48 +0100

    Fix up splash page

--- src/pages/Splash.tsx
+++ src/pages/Splash.tsx
@@ -1,13 +1,19 @@
 import { Link } from "react-router-dom";
 
+import styles from "./Splash.module.css";
+
 export default function SplashPage() {
   return (
-    <>
-      <h1>Paperboy Feed Reader</h1>
-      <p>Welcome to this very nice feed reader</p>
-      <p>
-        <Link to="/signin">Sign in</Link>
-      </p>
-    </>
+    <div className={styles.splash}>
+      <div className={styles.wrapper}>
+        <h1>Paperboy Feed Reader</h1>
+        <p>Welcome to this very nice feed reader</p>
+        <p>
+          <Link to="/signin" className="button">
+            Sign in
+          </Link>
+        </p>
+      </div>
+    </div>
   );
 }
