c08982d517d8e59e86854db6d3f44637e1eae29e
Lazar <lazar@fuzzylogic.ltd>
2025-02-25 15:20:24 +0100

    Make reading pane behave better during loading state

--- src/widgets/ReadingPane.tsx
+++ src/widgets/ReadingPane.tsx
@@ -5,30 +5,26 @@ import styles from "./ReadingPane.module.css";
 
 export default function ReadingPane({ articleId }) {
   const { isPending, isError, data, error } = useGetArticle(articleId);
-
-  if (isPending) {
-    return <span>Loading...</span>;
-  }
-
-  if (isError) {
-    return <span>Error: {error?.message}</span>;
-  }
-
-  const article = data.article;
-
   return (
     <div className={styles.readingPane}>
       <UserMenu />
       <div className={styles.wrapper}>
-        <h2>{article.title}</h2>
-        <h3>
-          {article.datePublished && article.datePublished}
-          {article.author && ` by ${article.author}`} -{" "}
-          <a href={article.url} target="_blank">
-            Original
-          </a>
-        </h3>
-        <div dangerouslySetInnerHTML={{ __html: article.body }} />
+        {isPending && articleId && <span>Loading...</span>}
+        {isError && <span>Error: {error?.message}</span>}
+
+        {data?.article && (
+          <>
+            <h2>{data.article.title}</h2>
+            <h3>
+              {data.article.datePublished && data.article.datePublished}
+              {data.article.author && ` by ${data.article.author}`} -{" "}
+              <a href={data.article.url} target="_blank">
+                Original
+              </a>
+            </h3>
+            <div dangerouslySetInnerHTML={{ __html: data.article.body }} />
+          </>
+        )}
       </div>
     </div>
   );
